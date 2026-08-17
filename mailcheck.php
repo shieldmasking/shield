986058d89eef23cdb74cc73964adf241d35452a5
Ryan <rstrenger@shieldmasking.com>
2026-08-17 09:30:18 -0500

    Add mail diagnostic script

---
+++ mailcheck.php
@@ -0,0 +1,7 @@
+<?php
+echo '<pre>';
+echo "sendmail_path: " . ini_get('sendmail_path') . "\n";
+echo "SMTP: "          . ini_get('SMTP') . "\n";
+echo "smtp_port: "     . ini_get('smtp_port') . "\n";
+echo "sendmail_from: " . ini_get('sendmail_from') . "\n";
+echo '</pre>';
